1a94ab3c6cfc54d10ef11150ed761da6f06f7be1
Takexin <ftrangel07@gmail.com>
2025-09-15 17:08:23 -0300

    commit final

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
 import json
+import time
 
 data = json.load(open("usuarios.json"))
+#print(data)
 
 def main():
     print("bem vindo ao sistema de login")
-    usuarioInput = input("Digite o usuario: ")
-    senhaInput = input("Digite a senha: ")
-
     tentativasCount = 3
-    senhaFound = False
     usuarioFound = False
 
-    while tentativasCount <= 0:
+    while tentativasCount > 0:
+        usuarioInput = input("Digite o usuario: ")
+        senhaInput = input("Digite a senha: ")
         tentativasCount -= 1 
+        usuarioIndex = -1
         for usuario in data:
-            if usuario["usuario"] == usuarioInput and usuario["senha"] == senhaInput:
+            usuarioIndex +=1
+            if usuario["usuario"] == usuarioInput:
                 usuarioFound = True
-                senhaFound = True
                 break
-            elif usuario["usuario"] == usuarioInput and usuario["senha"] != senhaInput:
-                usuarioFound = True
-                senhaFound = False
-            elif usuario["usuario"] != usuarioInput and usuario["senha"] == senhaInput:
-                usuarioFound = False 
-                senhaFound = True
-            else:
-                usuarioFound = False 
-                senhaFound = False
-    if senhaFound and usuarioFound:
-        print("Entrou no sistema") 
-    elif usuarioFound:
-        print("senha incorreta")
-    else:
-        print("Usuario nao encontrado")
+        #print(f"usuarioFound : {usuarioFound} senhaFound : {senhaFound}") 
+        if usuarioFound:
+            if data[usuarioIndex]["senha"] == senhaInput:
+                #print(data[usuarioIndex])
 
-def onLogin():
-    pass
+                print("Login realizado com sucesso!\n\n\n\n") 
+                tentativasCount = 3
+                onLogin(usuarioInput, senhaInput)
+                break
+            else:
+                print("Senha inválida\n\n\n\n")
+        else:
+            print("Usuario nao encontrado\n\n\n\n")
+    if(tentativasCount == 0):
+        print("Número máximo de tentativas excedido. Acesso bloqueado.\n\n\n\n")
+        time.sleep(30)
+        main()
+
+def onLogin(usuario, senha):
+    option = 0
+    while option != 3:
+        print("Selecione uma opção: \n1-Ver perfil \n2-Criar usuário \n3-Sair\n\n\n\n")
+        option = int(input("Opção:"))
+        match option:
+            case 1:
+                print(f"Usuário: {usuario} \n Senha: {senha}")
+
+            case 2:
+                usuarioRegistro = input("Digite um nome de usuário: ")
+                senhaRegistro = input("Digite uma senha: ")
+                data.append({"usuario" : usuarioRegistro, "senha" : senhaRegistro})
+                json.dump(data, open("usuarios.json", "w"), indent=4)
+                print("Usuário registrado!\n\n\n\n")
 main()
